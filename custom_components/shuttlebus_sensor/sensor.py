7bd275e4f1dea67167d25b656eba374ca620758a
Billy Chan <shing6326@gmail.com>
2023-12-11 22:44:35 +0800

    :rocket: big updates from code structure

--- custom_components/shuttlebus_sensor/sensor.py
+++ custom_components/shuttlebus_sensor/sensor.py
@@ -15,23 +15,11 @@ import json
 
 timezone = pytz.timezone('Asia/Hong_Kong')
 
-bus_schedules = {
-    "route_b": {
-        "non_holiday": [
-            ('06:35', 'B線'), ('06:35', 'B線 9座開出'), ('06:45', 'B線'), ('06:55', 'B線'), ('06:55', 'B線 9座開出'), ('07:05', '9座 開出'), ('07:10', 'B線'), ('07:15', 'B線'), ('07:20', 'B線'), ('07:20', 'B線 9座開出'), ('07:30', 'B線 9座開出'), ('07:35', 'B線'), ('07:40', 'B線'), ('07:45', 'B線'), ('07:50', 'B線 9座開出'), ('07:55', 'B線 9座開出'), ('08:00', 'B線'), ('08:05', 'B線'), ('08:10', 'B線'), ('08:20', 'B線 9座開出'), ('08:25', 'B線'), ('08:30', 'B線'), ('08:35', 'B線'), ('08:45', 'B線 9座開出'), ('08:50', 'B線'), ('08:55', 'B線'), ('09:05', 'B線'), ('09:15', 'B線'), ('09:25', 'B線'), ('09:35', 'B線'), ('09:45', 'B線'), ('09:55', 'B線'), ('10:05', 'B線'), ('10:20', 'B線'), ('10:35', 'B線'), ('10:50', 'B線'), ('11:05', 'B線'), ('11:20', 'B線'), ('11:35', 'B線'), ('11:50', 'B線'), ('12:05', 'B線'), ('12:20', 'B線'), ('12:35', 'B線'), ('12:50', 'B線'), ('13:05', 'B線'), ('13:20', 'B線'), ('13:30', 'B線'), ('13:40', 'B線'), ('13:50', 'B線'), ('14:00', 'B線'), ('14:10', 'B線'), ('14:20', 'B線'), ('14:30', 'B線'), ('14:40', 'B線'), ('14:50', 'B線'), ('15:00', 'B線'), ('15:10', 'B線'), ('15:20', 'B線'), ('15:30', 'B線'), ('15:40', 'B線'), ('15:50', 'B線'), ('16:00', 'B線'), ('16:10', 'B線'), ('16:20', 'B線'), ('16:30', 'B線'), ('16:40', 'B線'), ('16:55', 'B線'), ('17:10', 'B線'), ('17:20', 'B線'), ('17:35', 'B線'), ('17:45', 'B線'), ('18:00', 'B線'), ('18:10', 'B線'), ('18:20', 'B線'), ('18:25', 'B線'), ('18:35', 'B線'), ('18:45', 'B線'), ('18:50', 'B線'), ('19:00', 'B線'), ('19:10', 'B線'), ('19:15', 'B線'), ('19:25', 'B線'), ('19:35', 'B線'), ('19:40', 'B線'), ('19:50', 'B線'), ('20:05', 'B線'), ('20:15', 'B線'), ('20:30', 'B線'), ('20:40', 'B線'), ('20:55', 'B線'), ('21:05', 'B線'), ('21:20', 'B線'), ('21:30', 'B線'), ('21:45', 'B線'), ('21:55', 'B線'), ('22:10', 'B線'), ('22:20', 'B線'), ('22:30', 'B線'), ('22:40', 'B線'), ('22:50', 'B線'), ('23:00', 'B線'), ('23:15', 'B線 停全座')
-        ],
-        "holiday": [
-            ('06:35', 'B線'), ('06:45', 'B線'), ('06:55', 'B線'), ('07:05', 'B線'), ('07:15', 'B線'), ('07:25', 'B線'), ('07:35', 'B線'), ('07:45', 'B線'), ('07:55', 'B線'), ('08:05', 'B線'), ('08:15', 'B線'), ('08:25', 'B線'), ('08:35', 'B線'), ('08:45', 'B線'), ('08:55', 'B線'), ('09:05', 'B線'), ('09:15', 'B線'), ('09:25', 'B線'), ('09:35', 'B線'), ('09:45', 'B線'), ('10:00', 'B線'), ('10:15', 'B線'), ('10:30', 'B線'), ('10:45', 'B線'), ('11:00', 'B線'), ('11:15', 'B線'), ('11:30', 'B線'), ('11:45', 'B線'), ('12:00', 'B線'), ('12:10', 'B線'), ('12:15', 'B線'), ('12:25', 'B線'), ('12:35', 'B線'), ('12:45', 'B線'), ('12:55', 'B線'), ('13:00', 'B線'), ('13:10', 'B線'), ('13:20', 'B線'), ('13:25', 'B線'), ('13:35', 'B線'), ('13:45', 'B線'), ('13:50', 'B線'), ('14:00', 'B線'), ('14:15', 'B線'), ('14:25', 'B線'), ('14:40', 'B線'), ('14:55', 'B線'), ('15:10', 'B線'), ('15:25', 'B線'), ('15:40', 'B線'), ('15:50', 'B線'), ('16:00', 'B線'), ('16:10', 'B線'), ('16:20', 'B線'), ('16:30', 'B線'), ('16:40', 'B線'), ('16:50', 'B線'), ('17:00', 'B線'), ('17:10', 'B線'), ('17:20', 'B線'), ('17:25', 'B線'), ('17:35', 'B線'), ('17:45', 'B線'), ('17:50', 'B線'), ('18:00', 'B線'), ('18:10', 'B線'), ('18:15', 'B線'), ('18:25', 'B線'), ('18:35', 'B線'), ('18:40', 'B線'), ('18:50', 'B線'), ('19:05', 'B線'), ('19:15', 'B線'), ('19:30', 'B線'), ('19:40', 'B線'), ('19:55', 'B線'), ('20:05', 'B線'), ('20:20', 'B線'), ('20:30', 'B線'), ('20:45', 'B線'), ('20:55', 'B線'), ('21:10', 'B線'), ('21:20', 'B線'), ('21:35', 'B線'), ('21:45', 'B線'), ('21:55', 'B線'), ('22:05', 'B線'), ('22:15', 'B線'), ('22:25', 'B線'), ('22:35', 'B線'), ('22:45', 'B線'), ('23:00', 'B線'), ('23:15', 'B線 停全座')
-        ]
-    },
-    "route_c": {
-        "non_holiday": [
-            ('09:15', 'C線 9座開出'), ('10:15', 'C線 9座開出'), ('11:15', 'C線 9座開出'), ('12:15', 'C線 9座開出'), ('13:15', 'C線 9座開出'), ('14:15', 'C線 9座開出'), ('15:15', 'C線 9座開出'), ('16:15', 'C線 9座開出'), ('17:15', 'C線 9座開出'), ('20:15', 'C線 9座開出'), ('21:15', 'C線 9座開出'), ('22:15', 'C線 9座開出')
-        ],
-        "holiday": [
-            ('06:30', 'C線 9座開出'), ('07:30', 'C線 9座開出'), ('08:30', 'C線 9座開出'), ('09:30', 'C線 9座開出'), ('10:30', 'C線 9座開出'), ('11:30', 'C線 9座開出'), ('14:30', 'C線 9座開出'), ('15:30', 'C線 9座開出'), ('16:30', 'C線 9座開出'), ('19:30', 'C線 9座開出'), ('20:30', 'C線 9座開出'), ('21:30', 'C線 9座開出'), ('22:30', 'C線 9座開出')
-        ]
-    }
+bus_schedule = {
+    ('b', False): {'description': '南運路 非假日', 'schedule': [{'time': '06:35', 'info': 'B線'}, {'time': '06:35', 'info': 'B線 9座開出'}, {'time': '06:45', 'info': 'B線'}, {'time': '06:55', 'info': 'B線'}, {'time': '06:55', 'info': 'B線 9座開出'}, {'time': '07:05', 'info': '9座 開出'}, {'time': '07:10', 'info': 'B線'}, {'time': '07:15', 'info': 'B線'}, {'time': '07:20', 'info': 'B線'}, {'time': '07:20', 'info': 'B線 9座開出'}, {'time': '07:30', 'info': 'B線 9座開出'}, {'time': '07:35', 'info': 'B線'}, {'time': '07:40', 'info': 'B線'}, {'time': '07:45', 'info': 'B線'}, {'time': '07:50', 'info': 'B線 9座開出'}, {'time': '07:55', 'info': 'B線 9座開出'}, {'time': '08:00', 'info': 'B線'}, {'time': '08:05', 'info': 'B線'}, {'time': '08:10', 'info': 'B線'}, {'time': '08:20', 'info': 'B線 9座開出'}, {'time': '08:25', 'info': 'B線'}, {'time': '08:30', 'info': 'B線'}, {'time': '08:35', 'info': 'B線'}, {'time': '08:45', 'info': 'B線 9座開出'}, {'time': '08:50', 'info': 'B線'}, {'time': '08:55', 'info': 'B線'}, {'time': '09:05', 'info': 'B線'}, {'time': '09:15', 'info': 'B線'}, {'time': '09:25', 'info': 'B線'}, {'time': '09:35', 'info': 'B線'}, {'time': '09:45', 'info': 'B線'}, {'time': '09:55', 'info': 'B線'}, {'time': '10:05', 'info': 'B線'}, {'time': '10:20', 'info': 'B線'}, {'time': '10:35', 'info': 'B線'}, {'time': '10:50', 'info': 'B線'}, {'time': '11:05', 'info': 'B線'}, {'time': '11:20', 'info': 'B線'}, {'time': '11:35', 'info': 'B線'}, {'time': '11:50', 'info': 'B線'}, {'time': '12:05', 'info': 'B線'}, {'time': '12:20', 'info': 'B線'}, {'time': '12:35', 'info': 'B線'}, {'time': '12:50', 'info': 'B線'}, {'time': '13:05', 'info': 'B線'}, {'time': '13:20', 'info': 'B線'}, {'time': '13:30', 'info': 'B線'}, {'time': '13:40', 'info': 'B線'}, {'time': '13:50', 'info': 'B線'}, {'time': '14:00', 'info': 'B線'}, {'time': '14:10', 'info': 'B線'}, {'time': '14:20', 'info': 'B線'}, {'time': '14:30', 'info': 'B線'}, {'time': '14:40', 'info': 'B線'}, {'time': '14:50', 'info': 'B線'}, {'time': '15:00', 'info': 'B線'}, {'time': '15:10', 'info': 'B線'}, {'time': '15:20', 'info': 'B線'}, {'time': '15:30', 'info': 'B線'}, {'time': '15:40', 'info': 'B線'}, {'time': '15:50', 'info': 'B線'}, {'time': '16:00', 'info': 'B線'}, {'time': '16:10', 'info': 'B線'}, {'time': '16:20', 'info': 'B線'}, {'time': '16:30', 'info': 'B線'}, {'time': '16:40', 'info': 'B線'}, {'time': '16:55', 'info': 'B線'}, {'time': '17:10', 'info': 'B線'}, {'time': '17:20', 'info': 'B線'}, {'time': '17:35', 'info': 'B線'}, {'time': '17:45', 'info': 'B線'}, {'time': '18:00', 'info': 'B線'}, {'time': '18:10', 'info': 'B線'}, {'time': '18:20', 'info': 'B線'}, {'time': '18:25', 'info': 'B線'}, {'time': '18:35', 'info': 'B線'}, {'time': '18:45', 'info': 'B線'}, {'time': '18:50', 'info': 'B線'}, {'time': '19:00', 'info': 'B線'}, {'time': '19:10', 'info': 'B線'}, {'time': '19:15', 'info': 'B線'}, {'time': '19:25', 'info': 'B線'}, {'time': '19:35', 'info': 'B線'}, {'time': '19:40', 'info': 'B線'}, {'time': '19:50', 'info': 'B線'}, {'time': '20:05', 'info': 'B線'}, {'time': '20:15', 'info': 'B線'}, {'time': '20:30', 'info': 'B線'}, {'time': '20:40', 'info': 'B線'}, {'time': '20:55', 'info': 'B線'}, {'time': '21:05', 'info': 'B線'}, {'time': '21:20', 'info': 'B線'}, {'time': '21:30', 'info': 'B線'}, {'time': '21:45', 'info': 'B線'}, {'time': '21:55', 'info': 'B線'}, {'time': '22:10', 'info': 'B線'}, {'time': '22:20', 'info': 'B線'}, {'time': '22:30', 'info': 'B線'}, {'time': '22:40', 'info': 'B線'}, {'time': '22:50', 'info': 'B線'}, {'time': '23:00', 'info': 'B線'}, {'time': '23:15', 'info': 'B線 停全座'}]},
+    ('b', True): {'description': '南運路 假日', 'schedule': [{'time': '06:35', 'info': 'B線'}, {'time': '06:45', 'info': 'B線'}, {'time': '06:55', 'info': 'B線'}, {'time': '07:05', 'info': 'B線'}, {'time': '07:15', 'info': 'B線'}, {'time': '07:25', 'info': 'B線'}, {'time': '07:35', 'info': 'B線'}, {'time': '07:45', 'info': 'B線'}, {'time': '07:55', 'info': 'B線'}, {'time': '08:05', 'info': 'B線'}, {'time': '08:15', 'info': 'B線'}, {'time': '08:25', 'info': 'B線'}, {'time': '08:35', 'info': 'B線'}, {'time': '08:45', 'info': 'B線'}, {'time': '08:55', 'info': 'B線'}, {'time': '09:05', 'info': 'B線'}, {'time': '09:15', 'info': 'B線'}, {'time': '09:25', 'info': 'B線'}, {'time': '09:35', 'info': 'B線'}, {'time': '09:45', 'info': 'B線'}, {'time': '10:00', 'info': 'B線'}, {'time': '10:15', 'info': 'B線'}, {'time': '10:30', 'info': 'B線'}, {'time': '10:45', 'info': 'B線'}, {'time': '11:00', 'info': 'B線'}, {'time': '11:15', 'info': 'B線'}, {'time': '11:30', 'info': 'B線'}, {'time': '11:45', 'info': 'B線'}, {'time': '12:00', 'info': 'B線'}, {'time': '12:10', 'info': 'B線'}, {'time': '12:15', 'info': 'B線'}, {'time': '12:25', 'info': 'B線'}, {'time': '12:35', 'info': 'B線'}, {'time': '12:45', 'info': 'B線'}, {'time': '12:55', 'info': 'B線'}, {'time': '13:00', 'info': 'B線'}, {'time': '13:10', 'info': 'B線'}, {'time': '13:20', 'info': 'B線'}, {'time': '13:25', 'info': 'B線'}, {'time': '13:35', 'info': 'B線'}, {'time': '13:45', 'info': 'B線'}, {'time': '13:50', 'info': 'B線'}, {'time': '14:00', 'info': 'B線'}, {'time': '14:15', 'info': 'B線'}, {'time': '14:25', 'info': 'B線'}, {'time': '14:40', 'info': 'B線'}, {'time': '14:55', 'info': 'B線'}, {'time': '15:10', 'info': 'B線'}, {'time': '15:25', 'info': 'B線'}, {'time': '15:40', 'info': 'B線'}, {'time': '15:50', 'info': 'B線'}, {'time': '16:00', 'info': 'B線'}, {'time': '16:10', 'info': 'B線'}, {'time': '16:20', 'info': 'B線'}, {'time': '16:30', 'info': 'B線'}, {'time': '16:40', 'info': 'B線'}, {'time': '16:50', 'info': 'B線'}, {'time': '17:00', 'info': 'B線'}, {'time': '17:10', 'info': 'B線'}, {'time': '17:20', 'info': 'B線'}, {'time': '17:25', 'info': 'B線'}, {'time': '17:35', 'info': 'B線'}, {'time': '17:45', 'info': 'B線'}, {'time': '17:50', 'info': 'B線'}, {'time': '18:00', 'info': 'B線'}, {'time': '18:10', 'info': 'B線'}, {'time': '18:15', 'info': 'B線'}, {'time': '18:25', 'info': 'B線'}, {'time': '18:35', 'info': 'B線'}, {'time': '18:40', 'info': 'B線'}, {'time': '18:50', 'info': 'B線'}, {'time': '19:05', 'info': 'B線'}, {'time': '19:15', 'info': 'B線'}, {'time': '19:30', 'info': 'B線'}, {'time': '19:40', 'info': 'B線'}, {'time': '19:55', 'info': 'B線'}, {'time': '20:05', 'info': 'B線'}, {'time': '20:20', 'info': 'B線'}, {'time': '20:30', 'info': 'B線'}, {'time': '20:45', 'info': 'B線'}, {'time': '20:55', 'info': 'B線'}, {'time': '21:10', 'info': 'B線'}, {'time': '21:20', 'info': 'B線'}, {'time': '21:35', 'info': 'B線'}, {'time': '21:45', 'info': 'B線'}, {'time': '21:55', 'info': 'B線'}, {'time': '22:05', 'info': 'B線'}, {'time': '22:15', 'info': 'B線'}, {'time': '22:25', 'info': 'B線'}, {'time': '22:35', 'info': 'B線'}, {'time': '22:45', 'info': 'B線'}, {'time': '23:00', 'info': 'B線'}, {'time': '23:15', 'info': 'B線 停全座'}]},
+    ('c', False): {'description': '大埔延長線 非假日', 'schedule': [{'time': '09:15', 'info': 'C線 9座開出'}, {'time': '10:15', 'info': 'C線 9座開出'}, {'time': '11:15', 'info': 'C線 9座開出'}, {'time': '12:15', 'info': 'C線 9座開出'}, {'time': '13:15', 'info': 'C線 9座開出'}, {'time': '14:15', 'info': 'C線 9座開出'}, {'time': '15:15', 'info': 'C線 9座開出'}, {'time': '16:15', 'info': 'C線 9座開出'}, {'time': '17:15', 'info': 'C線 9座開出'}, {'time': '20:15', 'info': 'C線 9座開出'}, {'time': '21:15', 'info': 'C線 9座開出'}, {'time': '22:15', 'info': 'C線 9座開出'}]},
+    ('c', True): {'description': '大埔延長線 假日', 'schedule': [{'time': '06:30', 'info': 'C線 9座開出'}, {'time': '07:30', 'info': 'C線 9座開出'}, {'time': '08:30', 'info': 'C線 9座開出'}, {'time': '09:30', 'info': 'C線 9座開出'}, {'time': '10:30', 'info': 'C線 9座開出'}, {'time': '11:30', 'info': 'C線 9座開出'}, {'time': '14:30', 'info': 'C線 9座開出'}, {'time': '15:30', 'info': 'C線 9座開出'}, {'time': '16:30', 'info': 'C線 9座開出'}, {'time': '19:30', 'info': 'C線 9座開出'}, {'time': '20:30', 'info': 'C線 9座開出'}, {'time': '21:30', 'info': 'C線 9座開出'}, {'time': '22:30', 'info': 'C線 9座開出'}]}
 }
 
 # Global variable to store holiday data
@@ -67,7 +55,9 @@ async def check_and_refresh_holiday_data(hass):
 
 # Function to check if a given date is a holiday or a weekend (Saturday or Sunday)
 # This remains a synchronous function
-def is_holiday_or_weekend(date):
+def is_holiday_or_weekend():
+    now = datetime.now(timezone)
+    date = now.date()
     # Check if the day is Saturday or Sunday
     if date.weekday() == 5 or date.weekday() == 6:  # 5 is Saturday, 6 is Sunday
         return True
@@ -80,34 +70,32 @@ def is_holiday_or_weekend(date):
     return False
 
 # Function to get the next 'n' schedules based on the current time
-def get_next_schedules(route, day_type, current_time, n):
-    schedules = bus_schedules[route][day_type]
-    future_schedules = [time for time in schedules if time[0] > current_time]
+def get_next_schedules(route, is_holiday, current_time, n):
+    # Access the schedule data directly using the route and holiday status
+    schedule_data = bus_schedule.get((route, is_holiday), {}).get('schedule', [])
+    # Filter the future schedules based on the current time
+    future_schedules = [sched for sched in schedule_data if sched['time'] > current_time]
+    # Return the first 'n' future schedules
     return future_schedules[:n]
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-
     # Fetch and update holiday data at startup
     await fetch_and_update_holiday_data()
-
     # Schedule daily check for new holiday data
     hass.helpers.event.async_track_time_change(
         lambda now: check_and_refresh_holiday_data(hass),
         hour=0, minute=0, second=0
     )
-
     """Set up the sensor platform."""
     sensors = []
-
-    # Add title sensors
-    sensors.append(BusTitleSensor('route_b', hass))
-    sensors.append(BusTitleSensor('route_c', hass))
-
-    # Create 4 sensors for each shuttle route
-    for i in range(4):
-        sensors.append(BusScheduleSensor('route_b', i, hass))
-        sensors.append(BusScheduleSensor('route_c', i, hass))
-
+    # Retrieve unique routes from bus_schedule
+    unique_routes = set(route for route, _ in bus_schedule.keys())
+    # Add title sensors for each unique route
+    for route in unique_routes:
+        sensors.append(BusTitleSensor(route, hass))
+        # Create 4 schedule sensors for each route
+        for i in range(4):
+            sensors.append(BusScheduleSensor(route, i, hass))
     async_add_entities(sensors, True)  # The True at the end triggers an update upon entity addition
 
 class BusTitleSensor(SensorEntity):
@@ -118,7 +106,7 @@ class BusTitleSensor(SensorEntity):
         self.route = route
         self.hass = hass
         self._name = None
-        self.entity_id = f"sensor.shuttlebus_{self.route}_title"
+        self.entity_id = f"sensor.shuttlebus_route_{self.route}_title"
         self.update()
 
     @property
@@ -135,17 +123,11 @@ class BusTitleSensor(SensorEntity):
         """Sensor should not be polled."""
         return False
 
-    def generate_sensor_name(self):
-        """Generate the sensor name based on holiday status."""
-        now = datetime.now(timezone)
-        current_date = now.date()
-        route_name = "南運路" if self.route == 'route_b' else "大埔延長線"
-        holiday_status = "假日" if is_holiday_or_weekend(current_date) else "非假日"
-        return f"{route_name} {holiday_status}"
-
     def update(self):
         """Update the sensor."""
-        self._name = self.generate_sensor_name()
+        # Access the description directly using the route and holiday status
+        schedule_key = (self.route, is_holiday_or_weekend())
+        self._name = bus_schedule.get(schedule_key, {}).get('description', '')
         self.schedule_next_update()
 
     def schedule_next_update(self):
@@ -161,11 +143,11 @@ class BusScheduleSensor(Entity):
         self.route = route
         self.index = index
         self.hass = hass
-        self._name = None
-        self._icon = None
-        self._state = None
+        self._name = '⠀'
+        self._state = '⠀'
+        self._icon = '⠀'
         self._attributes = {}
-        self.entity_id = f"sensor.shuttlebus_{self.route}_{self.index + 1}"
+        self.entity_id = f"sensor.shuttlebus_route_{self.route}_{self.index + 1}"
         self.update()
 
     @property
@@ -196,16 +178,16 @@ class BusScheduleSensor(Entity):
         """Fetch new state data for the sensor."""
         now = datetime.now(timezone)
         current_time = now.strftime('%H:%M')
-        current_date = now.date()
-
-        # Determine if today is a holiday or non-holiday
-        is_holiday = is_holiday_or_weekend(current_date)
+        is_holiday = is_holiday_or_weekend()
 
         # Fetch the next schedules
-        next_schedules = get_next_schedules(self.route, 'holiday' if is_holiday else 'non_holiday', current_time, 4)
+        next_schedules = get_next_schedules(self.route, is_holiday, current_time, 4)
 
         if len(next_schedules) > self.index:
-            schedule_time_str, schedule_name = next_schedules[self.index]
+            schedule_item = next_schedules[self.index]
+            schedule_time_str = schedule_item['time']
+            schedule_info = schedule_item['info']
+
             schedule_time_naive = datetime.strptime(schedule_time_str, '%H:%M')
             schedule_time = now.replace(hour=schedule_time_naive.hour, minute=schedule_time_naive.minute, second=0, microsecond=0)
 
@@ -218,10 +200,10 @@ class BusScheduleSensor(Entity):
             seconds_diff = time_diff.total_seconds()
 
             # Update attributes
-            self._name = schedule_name
+            self._name = schedule_info
             self._icon = "mdi:bus"
             self._attributes['departure_time'] = schedule_time_str
-            self._attributes['route'] = self.route.upper()[-1]  # 'B' or 'C'
+            self._attributes['route'] = self.route
             self._attributes['is_holiday'] = is_holiday
 
             # Format the state display
@@ -237,9 +219,9 @@ class BusScheduleSensor(Entity):
                 self._state = f"{hours}:{minutes:02}小時"
         else:
             # No more schedules for the day
-            self._name = None  # Clear the name
-            self._state = None  # Clear the state
-            self._icon = None
+            self._name = '⠀'
+            self._state = '⠀'
+            self._icon = '⠀'
             self._attributes = {}
 
         # Schedule the next update
